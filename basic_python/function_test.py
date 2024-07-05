b1d060ef1fcc2e9ab014d0fec32cd85ed4bfec75
ervin.wang <zhiwen.wang@truckerpath.com>
2024-07-05 15:37:43 +0800

    测试函数

---
+++ basic_python/function_test.py
@@ -0,0 +1,28 @@
+
+
+# def change(a):
+#     print(id(a))   # 指向的是同一个对象
+#     print(a)
+#     a=10
+#     print(id(a))   # 一个新对象
+#     print(a)
+ 
+# a=1
+# print(id(a))
+# change(a)
+# print(a)
+
+
+
+ 
+# 可写函数说明
+# def changeme( mylist ):
+#    "修改传入的列表"
+#    mylist.append([1,2,3,4])
+#    print ("函数内取值: ", mylist)
+#    return
+ 
+# # 调用changeme函数
+# mylist = [10,20,30]
+# changeme( mylist )
+# print ("函数外取值: ", mylist)
